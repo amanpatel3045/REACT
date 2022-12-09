98dfdd92103cbc1cad3f14a71a907b11a0f168ba
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2022-12-09 21:15:17 +0530

    Create App.js

---
+++ Declarative Approach/App.js
@@ -0,0 +1,10 @@
+import Declarative from "./Components/Declarative";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+     <Declarative/>
+    </div>
+  );
+}
