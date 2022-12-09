40b3ad12042fcb6f2b938e8bccae1be15980f3be
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2022-12-09 21:25:24 +0530

    Create App.js

---
+++ Imperative/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  const para=document.createElement("p");
+  para.textContent="Imperative";
+  document.getElementById("root").append(para);
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
