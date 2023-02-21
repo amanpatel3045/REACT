631aa876c39697d227ce0e7cd6681d8b29c8cc04
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-02-21 21:28:25 +0530

    Create App.js

---
+++ cleanup Function/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Component from './Component'
+
+export default function App() {
+
+  return (
+    
+    <div className="App">
+      <Component/>
+    </div>
+  );
+}
