393b5dc219bf41951879d863d37c25d7997144d6
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-03-24 00:48:37 +0530

    done navbar and content on home page using sticky

---
+++ Sticky Navbar/src/App.js
@@ -0,0 +1,47 @@
+import TopBar from "./components/topbar/TopBar";
+
+function App() {
+  return (
+    <>
+     <TopBar />
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     <h1>Content bellow navbar</h1>
+     
+    </>
+  )
+ 
+
+}
+
+export default App;
---
+++ Sticky Navbar/src/components/topbar/TopBar.jsx
@@ -0,0 +1,5 @@
+import "./TopBar.css";
+
+export default function TopBar() {
+  return <div className="top">TopBar</div>;
+}
