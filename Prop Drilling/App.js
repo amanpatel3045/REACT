20117edf1a39a3491e8a6738b5a81d62d4af958c
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-04-25 22:52:12 +0530

    Delete App.js

--- Prop Drilling/App.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-import ParentComponent from './ParentComponent'
-export default function App() {
-  return (
-    <div className="App">
-     <ParentComponent/>
-    </div>
-  );
-}
