5bc591991be8b4fdd9daa3e1c30d27563bb258a9
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-04-25 22:50:56 +0530

    Create App.js

---
+++ Prop Drilling/src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import ParentComponent from './ParentComponent'
+export default function App() {
+  return (
+    <div className="App">
+     <ParentComponent/>
+    </div>
+  );
+}
