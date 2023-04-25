ca7141f72331154bcb787fd01273d068d272953a
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-04-25 22:54:55 +0530

    Create GrandChildComponent.jsx

---
+++ Prop Drilling/src/GrandChildComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GrandChildComponent = ({name}) => {
+  return (
+    <div>
+     
+      <p>Hello, {name}!</p>
+    </div>
+  );
+};
+
+export default GrandChildComponent;
